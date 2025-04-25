bb19f11f6dbf262490759c3f48b549702d82802b
Pratik1joshi <jcprakash555@gmail.com>
2025-04-25 16:56:15 +0545

    Third comit

--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
 "use client"
-import { useState, useEffect } from "react"
+import { useState, useEffect, Suspense } from "react"
 import Navbar from "@/components/navbar"
 import HeroSection from "@/components/hero-section"
 import FireAlerts from "@/components/fire-alerts"
